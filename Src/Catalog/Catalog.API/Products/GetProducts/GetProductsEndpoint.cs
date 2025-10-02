95e6c25d126b345e232cd8cf5eee548acdfb0f4c
asa <mikebraginsky24@gmail.com>
2025-10-02 16:11:47 +0300

    Get products added

--- Src/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ Src/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,29 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Catalog.API.Models;
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProducts
 {
     public class GetProductsEndpoint
     {
+
+
+        public record GetProductRequest();
+
+
+        public record GetProductResponse(IEnumerable<Product> Products);
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (ISender sender) =>
+            {
+
+                var result = await sender.Send(new GetProductQuery());
+                var response = result.Adapt<GetProductResponse>();
+                 return Results.Ok(response);
+
+
+
+            });
+        }
     }
 }
